56f7dbe22466b561b6ee7a9ee860394dc9621369
Kevin Han <thehyphenator9@gmail.com>
2011-11-03 17:20:43 -0500

    Nondescript

--- tilt.js
+++ tilt.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
     $('body').html('').append('<div id="rotateDiv"><iframe id="rotateFrame" src="' + window.location.href + '" \></div>');
     $('div#rotateDiv').css({
-        '-webkit-transform': 'rotate(5deg)',
-        '-moz-transform': 'rotate(5deg)',
-        '-o-transform': 'rotate(5deg)',
-        '-ms-transform': 'rotate(5deg)',
+        '-webkit-transform': 'rotate(2deg)',
+        '-moz-transform': 'rotate(2deg)',
+        '-o-transform': 'rotate(2deg)',
+        '-ms-transform': 'rotate(2deg)',
     });
     $('iframe#rotateFrame').css({
         'width': '100%',
