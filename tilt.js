1c1714eb3172bc89e62ff4580a8a0c485ebada2f
Kevin Han <thehyphenator9@gmail.com>
2011-11-03 17:18:19 -0500

    Try 2

--- tilt.js
+++ tilt.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
-    $('body').html('').append('<div id="rotateDiv"><iframe src="' + window.location.href + '" \></div>');
-    $('#rotateDiv').css({
+    $('body').html('').append('<div id="rotateDiv"><iframe id="rotateFrame" src="' + window.location.href + '" \></div>');
+    $('div#rotateDiv').css({
         '-webkit-transform': 'rotate(5deg)',
         '-moz-transform': 'rotate(5deg)',
         '-o-transform': 'rotate(5deg)',
-        '-ms-transform': 'rotate(5deg)'
+        '-ms-transform': 'rotate(5deg)',
+    });
+    $('iframe#rotateFrame').css({
+        'width': '100%',
+        'height': '100%'
     });
     alert('hello');
 });
