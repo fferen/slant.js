d1700d5d468ac38365c67132d20399334cdeed74
Kevin Han <thehyphenator9@gmail.com>
2011-11-03 22:10:35 -0500

    nada

--- tilt.js
+++ tilt.js
@@ -2,15 +2,15 @@ $(document).ready(function () {
     $('body').html('').append('<div id="rotateDiv"><iframe id="rotateFrame" src="' + window.location.href + '" \></div>');
     $('div#rotateDiv').css({
         'transform': 'rotate(0.5deg)',
-        'transform-origin': 'center',
+        'transform-origin': 'center center',
         '-webkit-transform': 'rotate(0.5deg)',
-        '-webkit-transform-origin': 'center',
+        '-webkit-transform-origin': 'center center',
         '-moz-transform': 'rotate(0.5deg)',
-        '-moz-transform-origin': 'center',
+        '-moz-transform-origin': 'center center',
         '-o-transform': 'rotate(0.5deg)',
-        '-o-transform-origin': 'center',
+        '-o-transform-origin': 'center center',
         '-ms-transform': 'rotate(0.5deg)',
-        '-ms-transform-origin': 'center',
+        '-ms-transform-origin': 'center center',
     });
     $('iframe#rotateFrame').css({
         'width': '100%',
